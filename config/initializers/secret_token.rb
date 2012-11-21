a21872155dcac449531152ddc5372025ded7de56
Matt Swain <swanmat524@gmail.com>
2012-11-21 13:33:23 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WedAssist::Application.config.secret_token = '7b247c898a33b16a6532a9689e75ad6d8f3178ed56bda18362a2b457458224ffff580857e7ebe9edb44da2035e36ab8bbe27643750315ab0628515dc92646f42'
